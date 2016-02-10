6f9f46767b95a899d4a371fc9bba4d5639bd87b2
Tony Boyles <Anthony@Boyles.cc>
2016-02-10 10:28:45 -0500

    Added some README stuff.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-Tony's RApid Plotting In R
-==========================
+TAPIR
+=====
+
+*Tony's Advanced Plotting In R*
+
+![Tapir](https://pixabay.com/static/uploads/photo/2013/07/12/18/44/tapir-153797_960_720.png)
+
+TAPIR is a rapid-plotting platform for the web. It does light-duty statistical aggregation, and about 90% of the standard library of statistical graphics. If you want a scatterplot in 30 seconds with no code, TAPIR is for you.
+
+It's mostly based on [Raptr](https://github.com/cargomoose/raptR), only with a vastly modified UI.
+
+OK, so what is *a* Tapir?
+-------------------------
+
+A Tapir is [this cute thing](https://en.wikipedia.org/wiki/Tapir).
--- server.R
+++ server.R
@@ -23,28 +23,28 @@ source('./global_constants.R')
 ## file size options
 # by default, the file size limit is 5MB. It can be changed by
 # setting this option. Here we'll raise limit to 10GB.
-options(shiny.maxRequestSize = 10*1024^3)
 # Note that this does not override other settings that could limit upload sizes.
 # For example, if this is deployed behind an nginx server, the server cap must also
 # be raised in order to allow larger file uploads.
+options(shiny.maxRequestSize = 10*1024^3)
 
 shinyServer(function(input, output, session) {
   ## reactive variables
-  source('./reactives/reactives.R', local=TRUE)  # general/miscellaneous
-  source('./reactives/dataset.R', local=TRUE)  # dataset variables
-  source('./reactives/plotWidgetVals.R', local=TRUE)  # plot widget values
-  source('./reactives/plotWidgetNames.R', local=TRUE)  # plot widget names
-  source('./reactives/plotWidgetOpts.R', local=TRUE)  # plot widget options
+  source('./reactives/reactives.R',              local=TRUE)  # general/miscellaneous
+  source('./reactives/dataset.R',                local=TRUE)  # dataset variables
+  source('./reactives/plotWidgetVals.R',         local=TRUE)  # plot widget values
+  source('./reactives/plotWidgetNames.R',        local=TRUE)  # plot widget names
+  source('./reactives/plotWidgetOpts.R',         local=TRUE)  # plot widget options
   source('./reactives/plotWidgetsDisplayCond.R', local=TRUE)  # plot widgets display condition
-  source('./reactives/plotWidgetsLoadedCond.R', local=TRUE)  # plot widgets load conditions
+  source('./reactives/plotWidgetsLoadedCond.R',  local=TRUE)  # plot widgets load conditions
   source('./reactives/plotWidgetSelectedVals.R', local=TRUE)  # plot widget selected values
-  source('./reactives/plot.R', local=TRUE)  # plot
+  source('./reactives/plot.R',                   local=TRUE)  # plot
   
   ## UI controls
   source('./uiWidgets/generalWidgets.R', local=TRUE)
-  source('./uiWidgets/fileWidgets.R', local=TRUE)
-  source('./uiWidgets/manAggWidgets.R', local=TRUE)
-  source('./uiWidgets/plotWidgets.R', local=TRUE)
+  source('./uiWidgets/fileWidgets.R',    local=TRUE)
+  source('./uiWidgets/manAggWidgets.R',  local=TRUE)
+  source('./uiWidgets/plotWidgets.R',    local=TRUE)
   
   ## download handlers
   source('./reactives/download.R', local=TRUE)
